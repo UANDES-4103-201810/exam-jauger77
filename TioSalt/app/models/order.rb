856ff41967d41e1795e58738af29154e4461ed31
jauger77 <jiauger@miuandes.cl>
2018-06-20 18:20:58 -0400

    fixed some mistakes

--- TioSalt/app/models/order.rb
+++ TioSalt/app/models/order.rb
@@ -1,4 +1,5 @@
 class Order < ApplicationRecord
+  belongs_to :customer
   has_many :pizzas
   validates :payment_method, presence: true
   validates :pizzas, :length => { :minimum => 1 }
--- TioSalt/config/routes.rb
+++ TioSalt/config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   resources :recipes, :crusts
   get 'home/index'
-  get '/recipes'
   root 'home#index'
 
   
