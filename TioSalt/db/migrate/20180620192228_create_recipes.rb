30e9bbbc400384dde9aa2fca55d31bb65b404a3a
jauger77 <jiauger@miuandes.cl>
2018-06-20 18:42:31 -0400

    database

--- TioSalt/db/migrate/20180620192228_create_recipes.rb
+++ TioSalt/db/migrate/20180620192228_create_recipes.rb
@@ -3,7 +3,7 @@ class CreateRecipes < ActiveRecord::Migration[5.1]
     create_table :recipes do |t|
       t.string :name
       t.integer :price
-      t.string :ingredient
+      t.integer :ingredient
 
       t.timestamps
     end
--- TioSalt/db/seeds.rb
+++ TioSalt/db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+recipes = Recipe.create([{}])
+ingredients = Ingredient.create([{id: '1', name: 'mozzarella'}, {id: '2',name: 'tomato'}, {id: '3',name: 'pepperoni'}, {id: '4',name: 'olives'}, {id: '5',name: 'onion'}, {id: '6',name:'artichokes'}, {id: '7',name:'mushrooms'}, {id: '8',name: 'ham'}, {name: 'shrimp'}])
